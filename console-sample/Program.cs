cc220c67be8121b5c8e1e78939a9403483074803
Eray Fırat <firateray@gmail.com>
2021-10-20 15:57:42 +0300

    console app

---
+++ console-sample/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace console_sample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Adınız:");
+            var name = Console.ReadLine();
+            Console.WriteLine("Soyadınız: ");
+            var surname = Console.ReadLine();
+            Console.WriteLine("Merhaba "+ name  + " " + surname);
+
+        }
+    }
+}
